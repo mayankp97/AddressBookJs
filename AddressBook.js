a1d67984b8474136fe0a1111d97b287df0f462db
Mayank Purohit <mayankpurohit2012@gmail.com>
2020-11-20 11:17:06 +0530

    [Mayank] Add validation for all keys

--- AddressBook.js
+++ AddressBook.js
@@ -13,6 +13,24 @@ function Contact(fName,lName,address,city,state,zip,phone,email){
     this.email = email;
 }
 
-let contact = new Contact('Mayank', 'Purohit','Street A','NY1','NY','548787','87878787','m@gmail.com');
+let contact = new Contact('Mayank', 'Purohit','Street A','NYEZ','NYSD','548787','98 9989898989','m@gmail.com');
 
 console.log(contact);
+
+//UC 2 : Validate
+let validate = {
+    fullName : fName => new RegExp('^[A-Z][A-Za-z]{2,}$').test(fName),
+    lastName : lName => new RegExp('^[A-Z][A-Za-z]{2,}$').test(lName),
+    city : city => new RegExp('^[A-Za-z]{4,}$').test(city),
+    state : state => new RegExp('^[A-Za-z]{4,}$').test(state),
+    zip : zip => new RegExp('^[0-9]{6}$').test(zip),
+    phone : phone => new RegExp('^[0-9]{2}[ ][1-9][0-9]{9}$').test(phone),
+    email : email => new RegExp('^[A-Za-z0-9]+([._+-][A-Za-z0-9]+)*[@][A-Za-z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3})?$').test(email),
+    validateAll : function(contact){
+        for(let key in contact)
+            if(key != 'address' && !validate[key](contact[key]))
+                throw key + ' is not valid';
+    }
+    };
+
+validate.validateAll(contact);
